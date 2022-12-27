8761a708db11d6650a622bf5f1d512cda6b0a52e
Aleksey <aleksey18rus@inbox.ru>
2022-12-27 15:02:27 +0300

    date parse

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ def Main():
 
     for i in range(1, len(argv), 1):
         currentFile = txtReader(argv[i])
+        j = 0
         while True:
+            j += 1
             line = currentFile.readline()
             if not line:
                 currentFile.close()
                 break
-            print(line)
+            parts = line.split(' ')
+            print(j, line)
+            print(parts[len(parts) - 2])
+            date = datetime.strptime(parts[3], "[%d/%b/%Y:%H:%M:%S")
+            print(date)
 
 
 if __name__ == "__main__":
