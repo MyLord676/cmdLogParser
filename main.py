495f83e3c6b5b1621764d2ccf94be5ce74fa1dfe
Aleksey <aleksey18rus@inbox.ru>
2022-12-28 11:44:31 +0300

    count and print statuse codes

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 from datetime import datetime
 from sys import argv
+import json
+import yaml
 
 from MyLibs.myTimeLib import round_time_floor
 from MyMockInterface.txtReader import txtReader
 
 
 def Main():
-    print(argv)
     if len(argv) <= 1:
         print("file not selected.")
         return
+    
+    with open("Configs/Config.yaml", "r") as stream:
+        try:
+            cfg = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    myDict: dict[float, dict[str, int]] = {}
 
     for i in range(1, len(argv), 1):
         currentFile = txtReader(argv[i])
-        j = 0
         while True:
-            j += 1
             line = currentFile.readline()
             if not line:
                 currentFile.close()
                 break
             parts = line.split(' ')
-            print(j, line)
-            print(parts[len(parts) - 2])
+            statusCode = parts[len(parts) - 2]
             date = datetime.strptime(parts[3], "[%d/%b/%Y:%H:%M:%S")
-            print(date)
+            roundDate = round_time_floor(date, cfg['grouping_depth'])
+            if roundDate in myDict:
+                if statusCode in myDict[roundDate]:
+                    myDict[roundDate][statusCode] += 1
+                else:
+                    myDict[roundDate][statusCode] = 1
+            else:
+                myDict[roundDate]: dict[str, int] = {}
+
+    sortDict = dict(sorted(myDict.items()))
+    for key in sortDict:
+        print(datetime.fromtimestamp(key), ": ")
+        print(json.dumps(sortDict[key], sort_keys=True, indent=4))
 
 
 if __name__ == "__main__":
